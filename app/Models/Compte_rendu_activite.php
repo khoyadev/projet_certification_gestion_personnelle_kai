70d8a57b7130f21778b98a567d19cc00631cb5ad
Rokhaya gueye <rokhydaaba63@gmail.com>
2022-07-13 12:04:48 +0000

    creation des migrations tables

---
+++ app/Models/Compte_rendu_activite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Compte_rendu_activite extends Model
+{
+    use HasFactory;
+}
